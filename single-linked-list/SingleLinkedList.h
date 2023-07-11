0c88f85446b37d52b8ee98b10b27244ddf787541
Natalia459 <125900645+Natalia459@users.noreply.github.com>
2023-07-11 17:02:20 +0300

    Add files via upload
    Sprint 7.3

--- single-linked-list/SingleLinkedList.h
+++ single-linked-list/SingleLinkedList.h
@@ -3,6 +3,7 @@
 #include <iterator>
 #include <initializer_list>
 #include <algorithm>
+#include <cassert>
 
 template <typename Type>
 class SingleLinkedList {
@@ -33,6 +34,7 @@ class SingleLinkedList {
 			node_ = other.node_;
 		}
 
+		// Чтобы компилятор не выдавал предупреждение об отсутствии оператора = при наличии
 		BasicIterator& operator=(const BasicIterator& rhs) = default;
 
 		// Категория итератора — forward iterator (итератор, который поддерживает операции инкремента и многократное разыменование)
@@ -69,30 +71,27 @@ class SingleLinkedList {
 
 		// Оператор прединкремента. После его вызова итератор указывает на следующий элемент списка
 		BasicIterator& operator++() noexcept {
-			if (this->node_) {
-				node_ = node_->next_node;
-				return *this;
-			}
+			assert(node_ != nullptr);
+			node_ = node_->next_node;
 			return *this;
 		}
 
 		// Оператор постинкремента. После его вызова итератор указывает на следующий элемент списка
 		BasicIterator operator++(int) noexcept {
-			if (this->node_) {
-				BasicIterator copy_node_(*this);
-				++(*this);
-				return copy_node_;
-			}
-			return *this;
+			BasicIterator copy_node_(*this);
+			++(*this);
+			return copy_node_;
 		}
 
 		// Операция разыменования. Возвращает ссылку на текущий элемент
 		[[nodiscard]] reference operator*() const noexcept {
+			assert(node_ != nullptr);
 			return node_->value;
 		}
 
 		// Операция доступа к члену класса. Возвращает указатель на текущий элемент списка
 		[[nodiscard]] pointer operator->() const noexcept {
+			assert(node_ != nullptr);
 			return &node_->value;
 		}
 
@@ -133,6 +132,7 @@ public:
 		return *this;
 	}
 
+	// Возвращает количество элементов в списке за время O(1)
 	[[nodiscard]] size_t GetSize() const noexcept {
 		return size_;
 	}
@@ -141,15 +141,18 @@ public:
 		return GetSize();
 	}
 
+	// Сообщает, пустой ли список за время O(1)
 	[[nodiscard]] bool IsEmpty() const noexcept {
 		return size_ == 0;
 	}
 
+	// Вставляет элемент value в начало списка за время O(1)
 	void PushFront(const Type& value) {
 		head_.next_node = new Node(value, head_.next_node);
 		++size_;
 	}
 
+	// Очищает список за время O(N)
 	void Clear() noexcept {
 		Node* prev_node = nullptr;
 		for (; head_.next_node != nullptr; prev_node = head_.next_node, head_.next_node = prev_node->next_node) {
@@ -162,33 +165,27 @@ public:
 		--size_;
 	}
 
+	// Обменивает содержимое списков за время O(1)
 	void swap(SingleLinkedList& other) noexcept {
 		if (this != &other) {
-			size_t temp_size = other.GetSize();
-
 			std::swap(other.head_.next_node, head_.next_node);
-
-			other.size_ = size_;
-			size_ = temp_size;
+			std::swap(size_, other.size_);
 		}
 	}
 
 	void PopFront() noexcept {
-		if (size_ != 0) { //извините, не совсем поняла ваш комментарий, вы написали: "лучше не умалчивать и по ассерт проверить", что лучше не умалчивать?
-			Node* first_node = head_.next_node;
-			head_.next_node = first_node->next_node;
-			delete first_node;
-			--size_;
-		}
+		assert(!IsEmpty());
+		Node* first_node = head_.next_node;
+		head_.next_node = first_node->next_node;
+		delete first_node;
+		--size_;
 	}
 
 	//Вставляет элемент value после элемента, на который указывает pos.
+	//Возвращает итератор на вставленный элемент
 	Iterator InsertAfter(ConstIterator pos, const Type& value) {
-		if (size_ == 0) {
-			Node* new_value = new Node(value, nullptr);
-			head_.next_node = new_value;
-			++size_;
-			return { Iterator(new_value) };
+		if ((size_ == 0) && (pos != this->before_begin())) {
+			assert(IsEmpty());
 		}
 
 		Node* prev_node = &head_;
@@ -208,6 +205,7 @@ public:
 	}
 
 	//Удаляет элемент, следующий за pos.
+	//Возвращает итератор на элемент, следующий за удалённым
 	Iterator EraseAfter(ConstIterator pos) noexcept {
 		if (size_ == 0) {
 			return Iterator(nullptr);
@@ -307,7 +305,7 @@ void swap(SingleLinkedList<Type>& lhs, SingleLinkedList<Type>& rhs) noexcept {
 
 template <typename Type>
 bool operator==(const SingleLinkedList<Type>& lhs, const SingleLinkedList<Type>& rhs) {
-	return std::equal(lhs.cbegin(), lhs.cend(), rhs.cbegin());
+	return (lhs.GetSize() == rhs.GetSize()) && std::equal(lhs.cbegin(), lhs.cend(), rhs.cbegin());
 }
 
 template <typename Type>
